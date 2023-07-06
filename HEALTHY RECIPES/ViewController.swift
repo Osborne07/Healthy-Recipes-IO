8eb53cf5ca1aeea8b42f7f9810b4d06870d1411c
nikhil <07nikhil.kamble@gmail.com>
2023-07-06 14:19:05 +0530

    Initial Commit

---
+++ HEALTHY RECIPES/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HEALTHY RECIPES
+//
+//  Created by Trycatch Classes on 06/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
